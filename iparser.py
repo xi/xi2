eb35c5ef644ffb1553ff2bc660d699358d3f6f38
Tobias Bengfort <tobias.bengfort@gmx.net>
2012-07-31 19:10:11 +0200

    init

---
+++ iparser.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+from midi import *
+
+"""
+The tricky part here is the time conversion.
+and noteOff events
+"""
+
+class IParser:
+
+	def __init__(self, seq, ch=0, offset=60):
+		self.midi = Midi()
+		self.ch = ch
+		self.offset = offset
+		self.dt = 0
+		self.dtStack = []
+		self.stack = []
+		self.parseSeq(seq)
+
+	def dtStep(self):
+		a = 1.0
+		for i in self.dtStack[1:]:
+			a /= i
+		return a	
+
+	def parseEl(self, e):
+		if e.isdigit(): # note
+			self.midi.noteOn(self.dt, self.ch, self.offset+int(e), 1)
+			self.dt = self.dtStep()
+		elif e == '-': # continue
+			self.dt += self.dtStep()
+		elif e == '': # break
+			self.dt += self.dtStep()
+		else: # lyrics
+			self.midi.lyrics(self.dt, e.replace('_', ' '))
+			self.dt = self.dtStep()
+
+	def parseSeq(self, seq):
+		self.dtStack.append(len(seq))
+		for e in seq:
+			if type(e) == type(''):
+				if e != '-': self.stop()
+				self.stack.append(e)
+				self.parseEl(e)
+			elif type(e) == type([]):
+				if not '-' in e: self.stop()
+				self.stack.append(e)
+				self.parseSet(e)
+			elif type(e) == type(()):
+				self.parseSeq(e)
+			else:
+				raise Exception("unknown element: " + e)
+		self.dtStack.pop()
+
+	def parseSet(self, s):
+		for e in s:
+			if type(e) != type(''):
+				raise Exception("only elements are allowed inside sets: " + e)
+			elif e == '':
+				raise Exception("Breaks are not allowed inside sets!")
+			else:
+				self.parseEl(e)
+			self.dt = 0
+		self.dt = self.dtStep()
+
+	def stop(self):
+		if len(self.stack) == 0: return
+		e = self.stack.pop()
+		if type(e) == type(''):
+			if e == '-':
+				self.stop()
+			elif e == '':
+				pass # already stopped
+			elif e.isdigit():
+				self.midi.noteOff(self.dt, self.ch, self.offset+int(e), 1)
+				self.dt = 0
+			else:
+				pass
+		elif type(e) == type([]):
+			if '-' in e:
+				self.stop()
+			for ee in e:
+				# we already checked the validity of the set when parsing.
+				# we only need to check if this is a note, lyrics or '-'
+				if ee.isdigit():	
+					self.midi.noteOff(self.dt, self.ch, self.offset+int(ee), 1)
+					self.dt = 0
+		else:
+			raise Exception("Unexpected object on stack: " + e)
+
+if __name__ == '__main__':
+	a = [(('0', '1'), '2'), '4', '5', '-', '', ['0', '4', '7'], '', '', '0', ['3', '-']]
+
+	f = MidiFile()
+	ip = IParser(a, 0, 60)
+	f.addTrack(ip.midi)
+	f.write('test.mid')
---
+++ xi2.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+
+import sys
+import re
+from iparser import IParser
+from midi import Midi, MidiFile
+from random import randint
+
+def length(data):
+	data = re.sub('{[^}]*}', '', data)
+	data = re.sub('\([^\)]*\)', '', data)
+	return len(data.split(',')) - 1	
+
+f = open(sys.argv[1])
+lines = f.readlines()
+f.close()
+
+ll = ''.join(lines)
+# remove whitespace
+ll = re.sub('[ \t]', '', ll)
+# remove c++ style comments
+# we have to escape linebreaks
+ll = re.sub('\n', '\\\\n', ll)
+ll = re.sub('/\*[^(\*)]*\*/', '', ll)
+ll = re.sub('\\\\n', '\n', ll)
+# remove c style comments
+ll = re.sub('//[^\n]*\n', '', ll)
+
+# expand macros
+ll = re.sub('\n', '\\\\n', ll)
+while re.search('(\[[^\]]*\]):(.*)', ll):
+	match = re.search('(\[[^\]]*\]):(.*)', ll)
+	(key, after) = match.groups()
+	if after.startswith('<<'):
+		eol = after.split('\\n',1)[0][2:]
+		val = after.split(eol,2)[1]
+		ll = re.sub(re.escape("%s:<<%s%s%s" % (key, eol, val, eol)), '', ll)
+	else:
+		val = after.split('\\n',1)[0]
+		ll = re.sub(re.escape("%s:%s" % (key, val)), '', ll)
+	ll = re.sub(re.escape(key), val, ll)
+ll = re.sub('\\\\n', '\n', ll)
+
+# remove trailing newlines
+ll = re.sub('^\n*', '', ll)
+# remove \n\n+
+ll = re.sub('\n', '\\\\n', ll)
+ll = re.sub('\\\\n(\\\\n)+', '\\\\n\\\\n', ll)
+ll = re.sub('\\\\n', '\n', ll)
+# remove newlines at the end
+ll = re.sub('\n*$', '', ll)
+
+print ll
+
+def parse(t):
+	string = '';
+	stack = [[]]
+	for c in t:
+		#print stack, string, c
+		if c == '{':
+			stack.append([])
+		elif c == '}':
+			stack[-1].append(string)
+			string = tuple(stack.pop())
+		elif c == '(':
+			stack.append([])
+		elif c == ')':
+			stack[-1].append(string)
+			string = stack.pop()
+		elif c == ',':
+			stack[-1].append(string)
+			string = ''
+		else:
+			string += c
+	return stack[0]
+
+# join track parts from different sets
+tracks = dict()
+for s in ll.split('\n\n'):
+	if len(tracks) == 0: l = 0
+	else: l = max([len(t) for t in tracks])
+	for track in s.split('\n'):
+		(name, data) = track.split(':', 1)
+		data = parse(data)
+		if not tracks.has_key(name):
+			tracks[name] = [''] * l
+		tracks[name] += data
+	if len(tracks) == 0: l = 0
+	else: l = max([len(t) for t in tracks])
+	for (name,data) in tracks.iteritems():
+		data += [''] * (l - len(data))
+
+# create midi
+mf = MidiFile()
+
+# create first track with meta infos
+m = Midi()
+m.setTempo(25)
+mf.addTrack(m)
+
+ch = 0
+for name, track in tracks.iteritems():
+	m = Midi()
+	# write meta info
+	m.metaEvent(0, 0x04, len(name), name)
+	try: prog = int(name)
+	except ValueError: prog = randint(0,127)
+	m.progCh(0, ch, prog)
+	# write data
+	ip = IParser(track, ch=ch, offset=76)
+	m += ip.midi
+	# write
+	mf.addTrack(m)
+	ch += 1
+
+# write to file
+mf.write('test.mid')
