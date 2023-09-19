4899d3cf474c0667c0405b60e13f16a4be74f898
Tobias Bengfort <tobias.bengfort@posteo.de>
2023-09-19 08:41:35 +0200

    style: high level restructure of xi2

--- xi2.py
+++ xi2.py
@@ -1,61 +1,17 @@
+"""Parse xi2 code and convert to MIDI."""
+
 import argparse
 import re
 
 import midi
 from iparser import IParser
 
-"""Parse xi2 code and convert to MIDI."""
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--tempo', help="tempo in bpm", default=120, type=int)
-parser.add_argument('-o', '--offset', help="key offset", default=60, type=int)
-parser.add_argument('infile')
-parser.add_argument('outfile')
-args = parser.parse_args()
 
 def length(data):
     data = re.sub('{[^}]*}', '', data)
     data = re.sub('\([^\)]*\)', '', data)
     return len(data.split(',')) - 1
 
-f = open(args.infile)
-lines = f.readlines()
-f.close()
-
-ll = ''.join(lines)
-# remove whitespace
-ll = re.sub('[ \t]', '', ll)
-# remove c++ style comments
-# we have to escape linebreaks
-ll = re.sub('\n', '\\\\n', ll)
-ll = re.sub('/\*[^(\*)]*\*/', '', ll)
-ll = re.sub('\\\\n', '\n', ll)
-# remove c style comments
-ll = re.sub('//[^\n]*\n', '', ll)
-
-# expand macros
-ll = re.sub('\n', '\\\\n', ll)
-while re.search('(\[[^\]]*\]):(.*)', ll):
-    match = re.search('(\[[^\]]*\]):(.*)', ll)
-    (key, after) = match.groups()
-    if after.startswith('<<'):
-        eol = after.split('\\n',1)[0][2:]
-        val = after.split(eol,2)[1]
-        ll = re.sub(re.escape("%s:<<%s%s%s" % (key, eol, val, eol)), '', ll)
-    else:
-        val = after.split('\\n',1)[0]
-        ll = re.sub(re.escape("%s:%s" % (key, val)), '', ll)
-    ll = re.sub(re.escape(key), val, ll)
-ll = re.sub('\\\\n', '\n', ll)
-
-# remove trailing newlines
-ll = re.sub('^\n*', '', ll)
-# remove \n\n+
-ll = re.sub('\n', '\\\\n', ll)
-ll = re.sub('\\\\n(\\\\n)+', '\\\\n\\\\n', ll)
-ll = re.sub('\\\\n', '\n', ll)
-# remove newlines at the end
-ll = re.sub('\n*$', '', ll)
 
 def parse(t):
     string = ''
@@ -78,51 +34,102 @@ def parse(t):
             string += c
     return stack[0]
 
-# join track parts from different sets
-tracks = dict()
-for s in ll.split('\n\n'):
-    if len(tracks) == 0:
-        l = 0
-    else:
-        l = max([len(t) for t in tracks])
-    for track in s.split('\n'):
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--tempo', help='tempo in bpm', default=120, type=int)
+    parser.add_argument('-o', '--offset', help='key offset', default=60, type=int)
+    parser.add_argument('infile')
+    parser.add_argument('outfile')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    with open(args.infile) as fh:
+        lines = fh.readlines()
+
+    ll = ''.join(lines)
+    # remove whitespace
+    ll = re.sub('[ \t]', '', ll)
+    # remove c++ style comments
+    # we have to escape linebreaks
+    ll = re.sub('\n', '\\\\n', ll)
+    ll = re.sub('/\*[^(\*)]*\*/', '', ll)
+    ll = re.sub('\\\\n', '\n', ll)
+    # remove c style comments
+    ll = re.sub('//[^\n]*\n', '', ll)
+
+    # expand macros
+    ll = re.sub('\n', '\\\\n', ll)
+    while re.search('(\[[^\]]*\]):(.*)', ll):
+        match = re.search('(\[[^\]]*\]):(.*)', ll)
+        (key, after) = match.groups()
+        if after.startswith('<<'):
+            eol = after.split('\\n',1)[0][2:]
+            val = after.split(eol,2)[1]
+            ll = re.sub(re.escape("%s:<<%s%s%s" % (key, eol, val, eol)), '', ll)
+        else:
+            val = after.split('\\n',1)[0]
+            ll = re.sub(re.escape("%s:%s" % (key, val)), '', ll)
+        ll = re.sub(re.escape(key), val, ll)
+    ll = re.sub('\\\\n', '\n', ll)
+
+    # remove trailing newlines
+    ll = re.sub('^\n*', '', ll)
+    # remove \n\n+
+    ll = re.sub('\n', '\\\\n', ll)
+    ll = re.sub('\\\\n(\\\\n)+', '\\\\n\\\\n', ll)
+    ll = re.sub('\\\\n', '\n', ll)
+    # remove newlines at the end
+    ll = re.sub('\n*$', '', ll)
+
+    # join track parts from different sets
+    tracks = dict()
+    for s in ll.split('\n\n'):
+        if len(tracks) == 0:
+            l = 0
+        else:
+            l = max([len(t) for t in tracks])
+        for track in s.split('\n'):
+            try:
+                (name, data) = track.split(':', 1)
+            except Exception as e:
+                print(track)
+                raise e
+            data = parse(data)
+            if name not in tracks:
+                tracks[name] = [''] * l
+            tracks[name] += data
+        if len(tracks) == 0:
+            l = 0
+        else:
+            l = max([len(t) for t in tracks])
+        for name, data in tracks.items():
+            data += [''] * (l - len(data))
+
+    # create first track with meta infos
+    midi_tracks = []
+    t0 = midi.Midi()
+    t0.set_tempo(args.tempo)
+    midi_tracks.append(t0)
+
+    ch = 0
+    for name, track in tracks.items():
+        m = midi.Midi()
+        # write meta info
+        m.meta_event_str(0, 0x04, name)
         try:
-            (name, data) = track.split(':', 1)
-        except Exception as e:
-            print(track)
-            raise e
-        data = parse(data)
-        if name not in tracks:
-            tracks[name] = [''] * l
-        tracks[name] += data
-    if len(tracks) == 0:
-        l = 0
-    else:
-        l = max([len(t) for t in tracks])
-    for name, data in tracks.items():
-        data += [''] * (l - len(data))
+            prog = int(name)
+        except ValueError:
+            prog = 0
+        m.prog_ch(0, ch, prog)
+        # write data
+        IParser(m, track, ch=ch, offset=args.offset)
+        # write
+        midi_tracks.append(m)
+        ch += 1
 
-# create first track with meta infos
-midi_tracks = []
-t0 = midi.Midi()
-t0.set_tempo(args.tempo)
-midi_tracks.append(t0)
-
-ch = 0
-for name, track in tracks.items():
-    m = midi.Midi()
-    # write meta info
-    m.meta_event_str(0, 0x04, name)
-    try:
-        prog = int(name)
-    except ValueError:
-        prog = 0
-    m.prog_ch(0, ch, prog)
-    # write data
-    IParser(m, track, ch=ch, offset=args.offset)
-    # write
-    midi_tracks.append(m)
-    ch += 1
-
-with open(args.outfile, 'wb') as fh:
-    midi.write_file(fh, midi_tracks)
+    with open(args.outfile, 'wb') as fh:
+        midi.write_file(fh, midi_tracks)
